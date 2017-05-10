a14b3f18abbf98ab44c38718cf077d943ea54d72
vineettiwari <me@vineettiwari.com>
2017-05-10 12:30:04 -0700

    Implemented input validations and function for 'Submit' button.

--- js/controllers.js
+++ js/controllers.js
@@ -3,17 +3,43 @@
 var validationApp = angular.module('validationApp', []);
 
 validationApp.controller('mainController', function ($scope) {
-
-    $scope.title = "My Form";
-
-    // function to submit the form after validation has occurred
-    $scope.submitForm = function (isValid) {
-
-        // check to make sure the form is completely valid
-        if (isValid) {
-            alert('our form is amazing');
-        }
-
+    $scope.title = "User Form";
+    $scope.emailError = "";
+    $scope.subjectError = "";
+    $scope.messageError = "";
+    $scope.submitted = false;
+    $scope.successMessage = "";
+    $scope.validateForm = function () {
+        console.log("processing...");
     };
 
+    $scope.submitForm = function (isValid) {
+        if (isValid) {
+            var email = $scope.userForm.email.$modelValue;
+            $scope.successMessage = "Your message has been delivered to " + email
+            console.log($scope.successMessage.$modelValue);
+            $scope.submitted = true;
+        } else {
+            if ($scope.userForm.email.$invalid) {
+                $scope.emailError = "Email is required.";
+                console.log("Email is required.");
+            }
+            if ($scope.userForm.subject.$invalid) {
+                $scope.subjectError = "Subject is required.";
+                console.log("Subject is required.");
+            }
+            if ($scope.userForm.message.$invalid) {
+                if ($scope.userForm.message.$error.minlength) {
+                    $scope.messageError = "Message must be at least 3 characters.";
+                    console.log("Message is too short.");
+                } else if ($scope.userForm.message.$error.maxlength) {
+                    $scope.messageError = "Message must be at most 255 characters.";
+                    console.log("Message is too long.");
+                } else {
+                    $scope.messageError = "Message is required.";
+                    console.log("Message is required.");
+                }
+            }
+        }
+    };
 });
