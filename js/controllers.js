96af0186718baff80b17c4232ae2dbd5e58a15b2
vineettiwari <me@vineettiwari.com>
2017-05-11 14:10:49 -0700

    Added Service to generate Success Message, also added comments above function implementations.

--- js/controllers.js
+++ js/controllers.js
@@ -1,43 +1,89 @@
-// controllers.js
+/*
+    controllers.js 
+ */
 
-var validationApp = angular.module('validationApp', []);
+// USER FORM APP
+var userFormApp = angular.module('userFormApp', []);
 
-validationApp.controller('mainController', function ($scope) {
+// USER FORM SERVICE
+userFormApp.service("userFormService", function () {
+
+    this.getSuccessMessage = function (userEmail) {
+        var successMessage = "Your message has been delivered to " + userEmail;
+        return successMessage;
+    };
+    
+});
+
+// USER FORM CONTROLLER
+userFormApp.controller('formController', function ($scope, userFormService) {
+
+    // INITIALIZE VARIABLES
     $scope.title = "User Form";
+
     $scope.emailError = "";
     $scope.subjectError = "";
     $scope.messageError = "";
-    $scope.submitted = false;
     $scope.successMessage = "";
-    $scope.validateForm = function () {
-        console.log("processing...");
-    };
 
+    $scope.submitted = false;
+
+    // VARIFY & SUBMIT FORM
     $scope.submitForm = function (isValid) {
+
+        console.log("validating...");
+
+        $scope.emailError = "";
+        $scope.subjectError = "";
+        $scope.messageError = "";
+        $scope.successMessage = "";
+
         if (isValid) {
+
+            console.log("Validation successful.");
+
             var email = $scope.userForm.email.$modelValue;
-            $scope.successMessage = "Your message has been delivered to " + email
-            console.log($scope.successMessage.$modelValue);
+
+            $scope.successMessage = userFormService.getSuccessMessage(email);
+            console.log($scope.successMessage);
+
             $scope.submitted = true;
+
         } else {
+
+            console.log("Validation failed.");
+
             if ($scope.userForm.email.$invalid) {
+
                 $scope.emailError = "Email is required.";
                 console.log("Email is required.");
+
             }
+
             if ($scope.userForm.subject.$invalid) {
+
                 $scope.subjectError = "Subject is required.";
                 console.log("Subject is required.");
+
             }
+
             if ($scope.userForm.message.$invalid) {
+
                 if ($scope.userForm.message.$error.minlength) {
+
                     $scope.messageError = "Message must be at least 3 characters.";
                     console.log("Message is too short.");
+
                 } else if ($scope.userForm.message.$error.maxlength) {
+
                     $scope.messageError = "Message must be at most 255 characters.";
                     console.log("Message is too long.");
+
                 } else {
+
                     $scope.messageError = "Message is required.";
                     console.log("Message is required.");
+
                 }
             }
         }
